eb6a41f23f04219e697084506b1cd32b83b9acc9
SlashRocky <32924435+SlashRocky@users.noreply.github.com>
2021-05-04 17:23:09 +0900

    スプレッド構文

--- src/index.js
+++ src/index.js
@@ -108,5 +108,53 @@
 /**
  * デフォルト値, 引数など
  */
-const sayHello = (name = "ゲスト") => console.log(`こんにちは!${name}さん`);
-sayHello("Rocky");
+// const sayHello = (name = "ゲスト") => console.log(`こんにちは!${name}さん`);
+// sayHello("Rocky");
+
+/**
+ * スプレッド構文 ...
+ */
+
+// 配列の展開
+// 配列に...が付いていたら順番に処理していると考えるべし！
+// const arr1 = [1, 2];
+// // console.log(arr1);
+// // console.log(...arr1);
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1);
+
+// まとめる
+const arr2 = [1, 2, 3, 4, 5];
+// const [num1, num2, num3, num4, num5] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+// console.log(num4);
+// console.log(num5);
+
+const [...arr3] = arr2;
+// console.log(num1);
+// console.log(num2);
+console.log(arr3);
+
+// 配列のコピー、結合
+const arr4 = [10, 20];
+const arr5 = [30, 40];
+
+const arr6 = [...arr4];
+arr6[0] = 100;
+console.log(arr6);
+console.log(arr4);
+
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7);
+
+// イコールでコピーしてしまうと同じところを参照してしまう。
+// const arr8 = arr4;
+// console.log(arr8);
+
+// arr8[0] = 100;
+// console.log(arr8);
+// console.log(arr4);
